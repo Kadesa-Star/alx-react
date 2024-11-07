cfb03ef6d9adea7feaf7b925627173602a1b9342
Kadesa-Star <kadesaondego@yahoo.com>
2024-11-07 21:22:43 +0300

    class components

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,10 +1,11 @@
-import React, { Component, Fragment } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Notifications from '../Notifications/Notifications';
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
-import Footer from '../Footer/Footer';
-import Notifications from '../Notifications/Notifications';
+import './App.css';
 import CourseList from '../CourseList/CourseList';
-import PropTypes from 'prop-types';
 import { getLatestNotification } from '../utils/utils';
 
 const listCourses = [
@@ -12,25 +13,29 @@ const listCourses = [
   { id: 2, name: 'Webpack', credit: 20 },
   { id: 3, name: 'React', credit: 40 },
 ];
+
 const listNotifications = [
   { id: 1, type: 'default', value: 'New course available' },
   { id: 2, type: 'urgent', value: 'New resume available' },
   { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
 ];
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
+class App extends React.Component {
   render() {
-    const { isLoggedIn } = this.props;
+    const { isLoggedIn } = this.props; // Destructuring to get isLoggedIn prop
     return (
-      <Fragment>
+      <>
         <Notifications listNotifications={listNotifications} />
-        <Header />
-        {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
-        <Footer />
-      </Fragment>
+        <div className="App">
+          <Header />
+        </div>
+        <div className="App-body">
+          {!isLoggedIn ? <Login /> : <CourseList listCourses={listCourses} />}
+        </div>
+        <div className="App-footer">
+          <Footer />
+        </div>
+      </>
     );
   }
 }
