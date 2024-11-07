d39185511623e662e4015592685a05955586c894
Kadesa-Star <kadesaondego@yahoo.com>
2024-11-07 22:23:56 +0300

    lifecycles

--- 0x03-React_component/task_1/dashboard/src/App/App.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -8,65 +8,63 @@ import "./App.css";
 import CourseList from "../CourseList/CourseList";
 import { getLatestNotification } from "../utils/utils";
 
+
 const listCourses = [
-  { id: 1, name: 'ES6', credit: 60 },
-  { id: 2, name: 'Webpack', credit: 20 },
-  { id: 3, name: 'React', credit: 40 }
-];
+  {id: 1, name: 'ES6', credit: 60},
+  {id: 2, name: 'Webpack', credit: 20},
+  {id: 3, name: 'React', credit: 40}
+]
 
 const listNotifications = [
-  { id: 1, type: 'default', value: 'New course available' },
-  { id: 2, type: 'urgent', value: 'New resume available' },
-  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
-];
+  {id: 1, type: 'default', value: 'New course available'},
+  {id: 2, type: 'urgent', value: 'New resume available'},
+  {id: 3, type: 'urgent', html: { __html: getLatestNotification() }}
+]
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.handlePress = this.handlePress.bind(this);
+    this.handlePress = this.handlePress.bind(this)
+
   }
 
   componentDidMount() {
-    document.addEventListener('keydown', this.handlePress);
+    document.addEventListener('keydown', this.handlePress)
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keydown', this.handlePress);
+    document.removeEventListener('keydown', this.handlePress)
   }
 
   handlePress(event) {
     if (event.ctrlKey && event.key === 'h') {
       event.preventDefault();
       alert('Logging you out');
-      this.props.logOut();
+      this.props.logOut()
     }
   }
 
-  render() {
-    return (
-      <>
-        <Notifications listNotifications={listNotifications} />
-        <div className="App">
-          <Header />
-        </div>
-        <div className="App-body">
-          {!this.props.isLoggedIn ? <Login /> : <CourseList listCourses={listCourses} />}
-        </div>
-        <div className="App-footer">
-          <Footer />
-        </div>
-      </>
+  render () {
+    return(  
+    <>
+      <Notifications listNotifications={listNotifications}/>
+      <div className="App">
+        <Header />
+      </div>
+      <div className="App-body">{!this.props.isLoggedIn ? <Login /> : <CourseList listCourses={listCourses}/>}</div>
+      <div className="App-footer">
+        <Footer />
+      </div>
+    </>
     );
   }
 }
 
-// Setting default props
 App.defaultProps = {
   isLoggedIn: false,
-  logOut: () => {} // Default empty function for logOut
+  logOut: () => undefined
 };
 
-// Setting prop types
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
   logOut: PropTypes.func,
