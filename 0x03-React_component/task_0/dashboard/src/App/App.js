bb9cf1e12beb5b18dd6411e8165661cffd45e188
Kadesa-Star <kadesaondego@yahoo.com>
2024-11-07 21:34:02 +0300

    class components

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,41 +1,39 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Notifications from '../Notifications/Notifications';
-import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
-import Login from '../Login/Login';
-import './App.css';
-import CourseList from '../CourseList/CourseList';
-import { getLatestNotification } from '../utils/utils';
+import React from "react";
+import PropTypes from "prop-types";
+import Notifications from "../Notifications/Notifications";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import "./App.css";
+import CourseList from "../CourseList/CourseList";
+import { getLatestNotification } from "../utils/utils";
+
 
 const listCourses = [
-  { id: 1, name: 'ES6', credit: 60 },
-  { id: 2, name: 'Webpack', credit: 20 },
-  { id: 3, name: 'React', credit: 40 },
-];
+  {id: 1, name: 'ES6', credit: 60},
+  {id: 2, name: 'Webpack', credit: 20},
+  {id: 3, name: 'React', credit: 40}
+]
 
 const listNotifications = [
-  { id: 1, type: 'default', value: 'New course available' },
-  { id: 2, type: 'urgent', value: 'New resume available' },
-  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
-];
+  {id: 1, type: 'default', value: 'New course available'},
+  {id: 2, type: 'urgent', value: 'New resume available'},
+  {id: 3, type: 'urgent', html: { __html: getLatestNotification() }}
+]
 
 class App extends React.Component {
-  render() {
-    const { isLoggedIn } = this.props; // Destructuring to get isLoggedIn prop
-    return (
-      <>
-        <Notifications listNotifications={listNotifications} />
-        <div className="App">
-          <Header />
-        </div>
-        <div className="App-body">
-          {!isLoggedIn ? <Login /> : <CourseList listCourses={listCourses} />}
-        </div>
-        <div className="App-footer">
-          <Footer />
-        </div>
-      </>
+  render () {
+    return(  
+    <>
+      <Notifications listNotifications={listNotifications}/>
+      <div className="App">
+        <Header />
+      </div>
+      <div className="App-body">{!this.props.isLoggedIn ? <Login /> : <CourseList listCourses={listCourses}/>}</div>
+      <div className="App-footer">
+        <Footer />
+      </div>
+    </>
     );
   }
 }
