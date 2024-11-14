8269770fcd639127e35318239bc89fdca12330c6
Kadesa-Star <kadesaondego@yahoo.com>
2024-11-14 20:16:52 +0300

    Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  const characters = movie.characters;
+
+  // Use Promise.all to handle asynchronous requests for characters in order
+  Promise.all(characters.map((characterUrl) => {
+    return new Promise((resolve, reject) => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          const character = JSON.parse(body);
+          resolve(character.name);
+        }
+      });
+    });
+  }))
+  .then((characterNames) => {
+    characterNames.forEach((name) => console.log(name));
+  })
+  .catch((error) => console.error('Error fetching character names:', error));
+});
