87a92248e508a66cebecdc785bb2560845e2c7a4
Kadesa-Star <kadesaondego@yahoo.com>
2024-11-20 21:43:53 +0300

    List and push

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,10 +1,11 @@
 import { List } from 'immutable';
 
 export function getListObject(array) {
-  //convertt the array into an immutable list
+  // Convert the array into an immutable List
   return List(array);
 }
-export function addElementToList(list, element){
-  // Append the string element to the List and return the new list
+
+export function addElementToList(list, element) {
+  // Append the string element to the List and return the new List
   return list.push(element);
 }
